8f65f4593a512cb3cbd8aa3dd20491a6207bc790
Ashley Allen <ashleytjallen@gmail.com>
2017-06-02 23:56:03 +0100

    Cleanup

--- src/mandelbrotset/MandelbrotSet.java
+++ src/mandelbrotset/MandelbrotSet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mandelbrotset;
 
 import java.io.File;
@@ -24,7 +19,7 @@ import javax.imageio.ImageIO;
 
 /**
  *
- * @author Ashley
+ * @author atja64
  */
 public class MandelbrotSet extends Application {
 
